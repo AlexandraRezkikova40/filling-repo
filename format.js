866f4f1c378a7be82ca224595d511c14c16b9b57
Alexandra Reznikova <154931161+AlexandraRezkikova40@users.noreply.github.com>
2023-12-26 23:43:48 +0200

    Create format.js

---
+++ format.js
@@ -0,0 +1,10 @@
+const settings =
+  typeof process.env.NEXT_PUBLIC_SETTINGS === 'string'
+    ? JSON.parse(process.env.NEXT_PUBLIC_SETTINGS)
+    : {}
+const nextConfig = {
+  compiler: {
+
+    settings
+  }
+}
